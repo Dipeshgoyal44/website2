a16acd888caa3cb5e69f66b09061dd7d6cfad850
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-04 19:15:19 +1100

    Cart'

--- js/app.js
+++ js/app.js
@@ -5,5 +5,10 @@ const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".cart-total")
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// Cart
+let cart = []
 
